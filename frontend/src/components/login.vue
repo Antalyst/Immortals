593a496e4d6a65f6cea7decbe3f33aee943c645e
Antalyst <delux385@gmail.com>
2024-12-04 13:29:20 +0800

    Your commit message here

---
+++ frontend/src/components/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="login" >
+    <input type="email" placeholder="log in" >
+    
+    <button type="submit">
+      submit
+    </button>
+    <input type="password" placeholder="password">
+    <button type="submit">
+      submit
+    </button>   
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
+<style>
+</style>
